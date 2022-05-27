0b23867664b37196da51dbc5028a9ba362f83566
SusanaChen03 <you@example.com>
2022-05-27 11:01:42 +0200

    CRUD create  new Reward

---
+++ app/Http/Controllers/RewardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Reward;
+
+class RewardController extends Controller
+{
+    
+    public function createReward(Request $request)
+    {
+        try {
+            Log::info('Init create reward');
+            $validator = Validator::make($request->all(), [   
+                'name' => 'required|string',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 418);
+            };
+
+            $newReward= new Reward();
+            $userId = auth()->user()->id;
+            $newReward->name = $request->name;
+            $newReward->user_id=$userId;  
+
+            $newReward->save();
+
+            return response()->json(["data"=>$newReward, "success"=>'Reward created'], 200);
+     
+        } catch (\Throwable $th) {
+            Log::error('Failed to create the Reward->'.$th->getMessage());
+
+            return response()->json([ 'error'=> 'Ups! Something wrong'], 500);
+        }
+    }
+
+    
+}
