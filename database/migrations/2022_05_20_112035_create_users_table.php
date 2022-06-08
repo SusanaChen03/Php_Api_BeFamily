0b495c9445265eed228be0ac20d2c2d8a914ebb5
SusanaChen03 <you@example.com>
2022-06-08 12:24:51 +0200

    UserTable add Rol

--- database/migrations/2022_05_20_112035_create_users_table.php
+++ database/migrations/2022_05_20_112035_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('rol');
             $table->timestamps();
         });
     }
