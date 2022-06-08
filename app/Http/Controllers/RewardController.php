6690c4029c17ee42401010f61c3067a40b5e536b
SusanaChen03 <you@example.com>
2022-06-08 12:22:18 +0200

    Update RewardController

--- app/Http/Controllers/RewardController.php
+++ app/Http/Controllers/RewardController.php
@@ -11,61 +11,77 @@ use App\Models\Reward;
 
 class RewardController extends Controller
 {
-    
+
     public function createReward(Request $request)
     {
         try {
             Log::info('Init create reward');
-            $validator = Validator::make($request->all(), [   
+            $validator = Validator::make($request->all(), [
                 'name' => 'required|string',
-                'image'=>'required|url',
-                'description'=>'required|string'
+                'image' => 'required|url',
+                'description' => 'required|string'
             ]);
 
             if ($validator->fails()) {
                 return response()->json($validator->errors(), 418);
             };
 
-            $newReward= new Reward();
+            $newReward = new Reward();
             $userId = auth()->user()->id;
             $newReward->name = $request->name;
             $newReward->image = $request->image;
             $newReward->description = $request->description;
-            $newReward->challenge_id= $request->challenge_id;
-           
 
             $newReward->save();
 
-            return response()->json(["data"=>$newReward, "success"=>'Reward created'], 200);
-     
+            return response()->json(["data" => $newReward, "success" => true], 200);
         } catch (\Throwable $th) {
-            Log::error('Failed to create the Reward->'.$th->getMessage());
+            Log::error('Failed to create the Reward->' . $th->getMessage());
 
-            return response()->json([ 'error'=> 'Ups! Something wrong'], 500);
+            return response()->json(['error' => 'Ups! Something wrong',  "success" => false], 500);
         }
     }
 
-    
-    public function getAllReward($challenge_id) //get all rewards for challenge id
+    public function getAllRewards()
     {
         try {
-            Log::info('start get all rewards');
+            Log::info('Get all rewards');
 
-            $reward = DB::table('rewards')->where('challenge_id', $challenge_id)->get()->toArray();
+            $reward = Reward::all();
 
             if (empty($reward)) {
-                return response()->json(["success"=> "There are not rewards"], 202);
+                return response()->json(
+                    ["success" => "There are not rewards"],
+                    202
+                );
+            };
+            return response()->json($reward, 200);
+        } catch (\Throwable $th) {
+            Log::error('Failed to get all reward->' . $th->getMessage());
+
+            return response()->json(['error' => 'Ups! Something wrong'], 500);
+        }
+    }
+
+    public function getRewardById($id)  //no funciona en el postman
+    {
+        try {
+            Log::info('start get rewards by Id');
+
+            $reward = DB::table('rewards')->where('id', $id)->get();
+
+
+            if (empty($reward)) {
+                return response()->json(["success" => "There are not rewards"], 202);
             };
 
             return response()->json($reward, 200);
-
         } catch (\Throwable $th) {
-            Log::error('Failed to get all rewards->'.$th->getMessage());
-            return response()->json(['error'=> 'Ups! Something Wrong'], 500);
+            Log::error('Failed to get all rewards->' . $th->getMessage());
+            return response()->json(['error' => 'Ups! Something Wrong' . $th], 500);
         }
     }
 
-    
     public function updateRewardById(Request $request, $id)
     {
         try {
@@ -74,33 +90,32 @@ class RewardController extends Controller
             $validator = Validator::make($request->all(), [
                 'name' => 'string|max:250',
                 'image' => 'url',
-                'description'=> 'string|max:500'
-            ]); 
+                'description' => 'string|max:500'
+            ]);
             if ($validator->fails()) {
                 return response()->json($validator->errors(), 418);
             };
 
             $reward = Reward::where('id', $id)->first();
 
-            if(empty($reward)){
-                return response()->json(['error'=>'rewards not exist'],404);
+            if (empty($reward)) {
+                return response()->json(['error' => 'rewards not exist'], 404);
             };
-            if(isset($request->name)){
+            if (isset($request->name)) {
                 $reward->name = $request->name;
             };
-            if(isset($request->image)){
+            if (isset($request->image)) {
                 $reward->image = $request->image;
             };
-            if(isset($request->description)){
+            if (isset($request->description)) {
                 $reward->description = $request->description;
             };
 
             $reward->save();
-            return response()->json(["data"=>$reward, "success"=>'Reward updated'], 200);
-
+            return response()->json(["data" => $reward, "success" => 'Reward updated'], 200);
         } catch (\Throwable $th) {
-            Log::error('Failed to update the reward->'.$th->getMessage());
-            return response()->json(['error'=> 'Ups! Something Wrong'], 500);
+            Log::error('Failed to update the reward->' . $th->getMessage());
+            return response()->json(['error' => 'Ups! Something Wrong'], 500);
         };
     }
 
@@ -109,53 +124,17 @@ class RewardController extends Controller
         try {
             Log::info('init delete reward');
 
-            $reward = Reward::where('id',$id)->first();
+            $reward = Reward::where('id', $id)->first();
 
-            if(empty($reward)){
-                return response()->json(['error'=>'Not found the reward'], 404);
+            if (empty($reward)) {
+                return response()->json(['error' => 'Not found the reward'], 404);
             }
 
             $reward->delete();
-            return response()->json(["data"=> "reward deleted"], 200);
-
+            return response()->json(["data" => "reward deleted"], 200);
         } catch (\Throwable $th) {
             Log::error('Failed to delete the Reward');
         }
     }
-
-    public function createRewardbyChallenge(Request $request) //???
-    {
-        try {
-            Log::info('init create reward by challenge');
-
-            $challenge = Challenge::find($request->challengeId);
-            $challenge->rewards()->attach($request->rewardId);
-
-            return response()->json(['data'=>'ok', 'success'=>'Reward created'], 200);
-        } catch (\Throwable $th) {
-            Log::error('Failed to create the Reward->'.$th->getMessage());
-            return response()->json([ 'error'=> $th->getMessage()], 404);
-        }
-    }
-
-
-//         public function getRewardById($id) 
-//     {
-//         try {
-//             Log::info('init get reward by id');
-//             $userId = auth()->user()->id;
-
-//             $reward = DB::table('rewards')->where('user_id', $userId)->where('user_id', $id)->get();
-
-//             if(empty($reward)){
-//                 return response()->json(['These not have rewards'], 404);
-//             };
-//             return response()->json($reward, 200);
-
-//         } catch (\Throwable $th) {
-//             Log::error('Failed to get reward by Id'.$th);
-//             return response()->json(['error'=> 'Ups! Somethings wrong'.$th], 500);
-//         }
-// }
 
 }
